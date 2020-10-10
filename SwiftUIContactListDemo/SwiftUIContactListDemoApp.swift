101af1584bcc8d43384ef2414337409f9e81df09
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-10-10 12:07:26 -0400

    Initial Commit

---
+++ SwiftUIContactListDemo/SwiftUIContactListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContactListDemoApp.swift
+//  SwiftUIContactListDemo
+//
+//  Created by gomathi saminathan on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContactListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
